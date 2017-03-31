9e8efde632b33844143b88b37f99fb20c9cfd979
Monica <mdkuo0@gmail.com>
2017-03-31 11:24:32 -0400

    platform work

--- src/prototype/Main.java
+++ src/prototype/Main.java
@@ -11,38 +11,39 @@ public class Main extends Game {
 	
 	PhysicsManager physicsMan;
 	ParticleManager particleMan;
-	static int gameWidth = 600;
-	static int gameHeight = 400;
+	static int gameWidth = 800;
+	static int gameHeight = 600;
 
 	public Main() {
 		super("Prototype", gameWidth, gameHeight);
 		physicsMan = new PhysicsManager();
-		Player player1 = new Player(false, 150, 200, this);
-		Player player2 = new Player(true, 450, 200, this);
+		Player player1 = new Player(false, (0.25) * gameWidth, 200, this);
+		Player player2 = new Player(true, (0.75) * gameWidth, 200, this);
 		player1.setOtherPlayer(player2);
 		player2.setOtherPlayer(player1);
 		physicsMan.addObject(player1);
 		physicsMan.addObject(player2); 
 		
-		// divider
-		// addPlatform(gameWidth/2-10, 0, 20, gameHeight);
-		addPlatform(gameWidth/2-10, 0, 20, 800);
+		// level
+		addPlatform(gameWidth/2, gameHeight/2, 20, gameHeight);
+		addPlatform(0, gameHeight/2, 20, gameHeight);
+		//addPlatform(gameWidth, gameHeight)
 		
 		addPlatform(100, 100, 100, 100);
 		
 		// particles
-		particleMan = new ParticleManager();
-		ParticleSystem test = new ParticleSystem("test");
-		test.create(10);
-		this.addChild(test);
-		particleMan.add(test);
+//		particleMan = new ParticleManager();
+//		ParticleSystem test = new ParticleSystem("test");
+//		test.create(10);
+//		this.addChild(test);
+//		particleMan.add(test);
 	}
 	
 	@Override
 	public void update(ArrayList<Integer> pressedKeys) {
 		super.update(pressedKeys);
 		physicsMan.update();
-		particleMan.update();
+//		particleMan.update();
 	}
 
 	private Platform addPlatform(double x, double y, double width, double height) {
